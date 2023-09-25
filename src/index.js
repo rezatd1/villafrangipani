f04f333dc3b73746d92e32737dde04954d98c04a
rezatd <reza.torkashvand.ins@gmail.com>
2023-09-24 20:45:17 -0700

    set the base of navbar

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min'; 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import App from './App';
+import CheckDates from './views/availability/checkDates/CheckDates';
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/availability" element={<CheckDates />}>
+        <Route path="check-dates" element={<CheckDates />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById('root')
 );
---
+++ src/views/availability/checkDates/CheckDates.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Navbar from '../../../components/navbar/Navbar';
+
+export default function CheckDates() {
+  return (
+    <div className='bg-success' style={{ width: '100vw', height: '100vh' }}>
+      <Navbar />
+    </div>
+  )
+}
