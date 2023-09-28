6e2171c3eeed9ce9e833ec66d45bc604b96be121
rezatd <reza.torkashvand.ins@gmail.com>
2023-09-28 08:50:35 -0700

    c t axios

--- src/components/calendar/Calendar.js
+++ src/components/calendar/Calendar.js
@@ -3,6 +3,7 @@ import './Calendar.scss';
 import CommonHelper from '../../utilities/commonHelper/CommonHelper';
 import rightArrow from '../../assets/img/right-arrow-15-black.png';
 import leftArrow from '../../assets/img/left-arrow-15-black.png';
+import axios from 'axios';
 
 const Calendar = () => {
     const [date, setDate] = useState(new Date());
@@ -106,24 +107,22 @@ const Calendar = () => {
     };
 
     const calendarEventChecker = () => {
-        fetch(
-            '//46.249.102.250:9999/api/api.php?MOD=462&AGENT=OWNER_KENT_552_1370&VILLA=1370&NAME=FRANGIPANI'
-        )
-            .then((response) => {
-                response.text();
-                console.log('***response', response.text());
-            })
-            .then((icalData) => {
-                const formattedEvents = parseICalendarData(icalData);
-                console.log('***icalData', icalData);
-                setEvents(formattedEvents);
-                setIsLoading(false);
-            })
-            .catch((error) => {
-                console.log(error);
-                setIsLoading(false);
-            });
-    };
+        axios
+          .get(
+            'http://46.249.102.250:9999/api/api.php?MOD=462&AGENT=OWNER_KENT_552_1370&VILLA=1370&NAME=FRANGIPANI'
+          )
+          .then((response) => {
+            const icalData = response.data;
+            console.log('***icalData', icalData);
+            const formattedEvents = parseICalendarData(icalData);
+            setEvents(formattedEvents);
+            setIsLoading(false);
+          })
+          .catch((error) => {
+            console.log(error);
+            setIsLoading(false);
+          });
+      };
 
     return (
         <div className="w-100 d-flex justify-content-center">
